c7f80af2ad95e3eb743c188a22b86007849c48c2
Flaminel <flm.marius@yahoo.com>
2021-08-15 17:31:31 +0300

    modified template

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
-const core = require('@actions/core');
-const wait = require('./wait');
+const core = require("@actions/core");
+const fetch = require("node-fetch");
 
-
-// most @actions toolkit packages have async methods
 async function run() {
-  try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    try {
+        const headers = {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+        };
+        const body = {
+            accessToken: core.getInput("access-token"),
+            secretsKeys: JSON.parse(core.getInput("secrets-names")).names,
+        };
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+        const response = await fetch(
+            "https://secrets-api.pricegrabber.xyz/api/secrets/multiple",
+            {
+                method: "POST",
+                body: JSON.stringify(body),
+                headers: headers,
+            }
+        );
 
-    core.setOutput('time', new Date().toTimeString());
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+        if (response.status === 404) {
+            core.setFailed(await response.json());
+            return;
+        }
+
+        if (response.status !== 200) {
+            core.setFailed("Failed with status " + response.status);
+            return;
+        }
+
+        const content = await response.json();
+
+        core.setOutput("secrets-values", JSON.stringify(content));
+    } catch (error) {
+        core.setFailed(error.message);
+    }
 }
 
 run();
